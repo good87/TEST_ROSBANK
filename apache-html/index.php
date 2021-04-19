69423cd86427da66509b1345ba6e2b904a0a6920
good87 <nicolay686@gmail.com>
2021-04-20 01:54:13 +0700

    7th_version

--- apache-html/index.php
+++ apache-html/index.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TASK_2</title>
-</head>
-<body>
-    <h1>
-        ROSBANK
-    </h1>
-
-	<h2>
-		<?php
-		$hostname = gethostbyaddr($_SERVER['SERVER_ADDR']);
-		echo $hostname;
-		?>
-	</h2>
-</body>
-</html>
+<?php
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Cancel';
+    exit;
+} else {
+    echo "<h2> ROSBANK{$_SERVER['PHP_AUTH_USER']}</h2>";
+    $hostname = gethostbyaddr($_SERVER['SERVER_ADDR']);
+    echo $hostname;
+}
+?>
